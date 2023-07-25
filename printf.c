5f7d306893d6560a6f10f85653c790f0068ae73e
robelinline <robelamare0@gmail.com>
2023-07-25 15:58:06 -0700

    betty fi

--- printf.c
+++ printf.c
@@ -7,6 +7,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * @format: format.
  * Return: Printed chars.
  */
+
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
@@ -53,9 +54,9 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
+ * print_buffer - Prints the co
  * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * @buff_ind: Index at which .
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
