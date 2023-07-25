3f64fcbbd4b016276fa4723146ee645db64893dc
MirewK <133372762+MirewK@users.noreply.github.com>
2023-07-26 02:16:31 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -5,7 +5,7 @@
  * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
+ * @flags: Calculates active flags if any
  * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
